90d453ad58b60bb34533efd5063721131ab4192c
MarkHiga <mark.higa@gmail.com>
2012-10-20 09:25:28 -0400

    SpeechToTextSample
    initial commit of speech to text sample

---
+++ SpeechToTextSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿// -
+// <copyright file="AssembkyInfo.cs" company="Microsoft Corporation">
+//    Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Speech To Text Sample App")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf7c8d33-4b74-4e2c-b241-0d28f904bbbf")]
+ 
