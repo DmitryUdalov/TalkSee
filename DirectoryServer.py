8e986bc7249629662609b956851ab9bc90c4112a
cbauschk <bauschkac@gmail.com>
2012-11-27 12:45:06 -0500

    The directory server and phone now have basic functionality.

--- DirectoryServer.py
+++ DirectoryServer.py
@@ -13,41 +13,37 @@ import subprocess
 # username number status
 testuser = "Ashley 578-245-1234 busy"
 testuser2 = "Chris 234-123-1234 available"
+testuser3 = "Mark 234-123-1234 busy"
+testuser4 = "Paul 234-123-1234 busy"
+testuser5 = "Steve 234-123-1234 available"
 
 # this is run each time the server gets a TCP packet
 class MyTCPHandler(SocketServer.StreamRequestHandler):
 
     def handle(self):
         self.data = self.rfile.readline().strip()
-        print "%s wrote: " % self.client_address[0]
+        #print "%s wrote: " % self.client_address[0]
         print self.data
         if(self.data[:6] == "REMOVE" and self.data[:7] != "REFRESH"):
             removeUser(self.data[7:])
         elif(self.data[:3] == "ADD"):
             addUser(self.data[4:])
-        #i = 0
-        #for user in ipuserlist:
-            #i = i+1
-        #print i
-        #j = 0
-        for user in ipuserlist:
-            #j = j + 1
-            print user
-            #if j == i:
-                #self.wfile.write(user)
-            #else:
-            self.wfile.write(user + "|")
+        #print "SENDING"
+        ipuserlist.sort()
+        print ipuserlist
+        self.wfile.write(ipuserlist)
 
 # add a user
 def addUser(toAdd):
+    print toAdd
     if (toAdd) not in ipuserlist and not toAdd=="":
         ipuserlist.append(toAdd)
 
 # remove a user
 def removeUser(toRemove):
-    #print toRemove
-    if toRemove in ipuserlist:
-      ipuserlist.remove(toRemove)
+    print toRemove
+    if toRemove in ipuserlist and not toRemove=="":
+        ipuserlist.remove(toRemove)
 
 def main():
     if(len(sys.argv) == 2):
@@ -64,23 +60,26 @@ def main():
 
 # print out the directory list periodically
 def server_print():
-    while(1):
-        time.sleep(5)
-        print "----------"
-        for user in ipuserlist:
-          print user
-        #print ipuserlist
-        print "----------"
+    pass
+    #while(1):
+    #    time.sleep(5)
+    #    print "----------"
+    #    for user in ipuserlist:
+    #      print user
+    #    print "----------"
 
 ipuserlist = []
-try:
-    thread.start_new_thread(server_print, ())
-except:
-    print "Error: unable to start thread"
+#try:
+#    thread.start_new_thread(server_print, ())
+#except:
+#    print "Error: unable to start thread"
 
 # start the server
 # add testuser
 addUser(testuser)
 addUser(testuser2)
+addUser(testuser3)
+addUser(testuser4)
+addUser(testuser5)
 main()
 
